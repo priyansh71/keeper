e8609b0e14957eae953478a0587670545ab83815
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-15 00:25:23 +0530

    made slight changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ const App = () =>{
                 <Route path="/" component={Home} />
             </Switch>
         </Router>
-        
         </div>
 
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Todo from "./components/Todo"
 
 ReactDOM.render(<App />, document.getElementById("root"));
