668fe1f62bca251b206b8e73b2cebdba8a397c62
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-14 17:12:06 +0530

    changed static content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "../public/styles.css";
+import App from "./App";
+import Todo from "./components/Todo"
 
 ReactDOM.render(<App />, document.getElementById("root"));
