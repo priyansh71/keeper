a340c69173a6e6010e9d24206544a7c2b3f882ea
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-16 15:39:22 +0530

    managed css on main components

--- src/ToDo/ToDo.jsx
+++ src/ToDo/ToDo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./styles.css";
+import "./index.css";
 import InputArea from "./InputArea";
 import ToDoItem from "./ToDoItem";
 
@@ -21,20 +21,18 @@ export default function Todo() {
   })
  }
   return (
-    <center>
-    <div className="container" id="fancyscroll">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
+    <div id="fancyscroll" className=" sm:w-5/6 lg:w-3/5 h-screen mt-24 mb-0 mx-auto border-white py-10 text-3xl float-none text-gray-900 bg-transparent">
+        <h1 className="mt-4 mb-5 text-3xl text-center">To-Do List</h1>
      <InputArea submit={handlesubmit} />
       <div>
         <ul>
           {A.map((item,index) => (
+            <div className="list-disc text-2xl my-2 sm:mx-40 lg:mx-52">
            <ToDoItem key={index} id={index} text={item} onRemove={handleremove} />
+           </div>
           ))}
         </ul>
       </div>
     </div>
-    </center>
   );
 }
