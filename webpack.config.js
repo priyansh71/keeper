3f3ad49b64620ed24046ba6c768e17beca19df4f
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-17 14:57:59 +0530

    --allow-empty

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: 'index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin({
-      minify: false
-  })],
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+        minify : false
+    })
+  ]
+}
