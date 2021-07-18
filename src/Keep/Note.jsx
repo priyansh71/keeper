75a7dcbc06ff76819ab0f1627f768e637383959e
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-18 18:58:08 +0530

    Added note creation time

--- src/Keep/Note.jsx
+++ src/Keep/Note.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
+import { Card } from "@material-ui/core";
+
+const date = new Date();
+const hour = date.getHours();
+const minutes = date.getMinutes();
 
 function Note(props) {
   return (
-    <div className="note">
+    <Card className="note" raised={true}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <Zoom in={true} timeout={{ enter : "400ms"}}>
@@ -18,7 +23,8 @@ function Note(props) {
         <DeleteOutlinedIcon style={{ color : "#e91e63" , fontSize : "1.82em"}} />
       </Fab>
       </Zoom>
-    </div>
+      <span>{hour}:{minutes} </span>
+    </Card>
   );
 }
 
