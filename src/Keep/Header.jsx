7670ced2fe5ba161015b5ea5f61c0f7474757144
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-07-25 20:13:34 +0530

    updated header and background

--- src/Keep/Header.jsx
+++ src/Keep/Header.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import GitHubIcon from '@material-ui/icons/GitHub';
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function Header() {
   return (
     <header>
-      <span>Keeper</span>
-      <span style={{position: "absolute", top: "20px", right : "15px"}}>
-        <a href="https://github.com/priyansh71" target="_blank" style={{textDecoration: null , color: "white"}}>
+      <center>
+        <span>Keeper</span>
+      </center>
+      <span
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "15px",
+          fontSize: "2em",
+        }}
+      >
+        <a
+          href="https://github.com/priyansh71"
+          target="_blank"
+          style={{ textDecoration: null, color: "black" }}
+        >
           <GitHubIcon fontSize="large" />
-          </a>
+        </a>
       </span>
     </header>
   );
